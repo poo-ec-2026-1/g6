897de2cf526484636745df86d1245d070c80e26b
Ittallo <ittallianu@gmail.com>
2026-05-27 11:58:05 -0300

    Delete ServicoTransporte.java

--- ServicoTransporte.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-// ===== CONCEITO DE POO: INTERFACE (CONTRATO) =====
-public interface ServicoTransporte {
-    // Nome bem claro e idêntico para todo o sistema
-    List<Previsao> consultarPrevisoesDoPonto(String codigoDoPonto);
-}
